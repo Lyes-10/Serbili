6907f3f63ca86d3a0d46c9a133ba533a56d34ec1
Axio-matrix <a.khedari@esi-sba.dz>
2025-03-01 16:03:35 +0100

    Adding the accessToken to the cookies

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -28,22 +28,23 @@ const refreshToken = asyncWrapper(async (req, res) => {
   if (!user) {
     throw new NotFoundError("User not found");
   }
-  //delete the old one
-  await db.refreshToken.deleteToken(tokenRefresh, user.id);
+
   //generate the new access token
   const newAccessToken = await user.generateAccessToken();
   //generate the new refresh token
-  const newRefreshToken = await db.refreshToken.generateToken(user.id);
-
+  res.cookie('accessToken', newAccessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'Strict',
+    maxAge: process.env.COOKIE_AGE, // 15 minutes
+  });
   return res.status(200).json({
-    accessToken: newAccessToken,
-    refreshToken: newRefreshToken.token,
+    msg: "Token refreshed successfully",
   });
 });
 const register = asyncWrapper(async (req, res) => {
   const { firstname, lastname, email, password, userType, phoneNumber } =
     req.body;
-  console.log(req.body);
   if (
     !firstname ||
     !lastname ||
@@ -75,7 +76,6 @@ const register = asyncWrapper(async (req, res) => {
 
 const login = asyncWrapper(async (req, res) => {
   const { identifier, password } = req.body;
-  console.log(req.body);
   if (!identifier || !password) {
     throw new BadRequestError("Please provide phone or email and password");
   }
@@ -96,10 +96,15 @@ const login = asyncWrapper(async (req, res) => {
   }
   const accessToken = await user.generateAccessToken();
   const refreshToken = await db.refreshToken.generateToken(user.id);
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+    sameSite: "Strict",
+    maxAge: process.env.COOKIE_AGE, // 15 minutes (same as access token expiration)
+  });
   return res.status(200).json({
     message: "User logged in successfully",
     user,
-    accessToken,
     refreshToken: refreshToken.token,
   });
 });
@@ -114,6 +119,12 @@ const logout = asyncWrapper(async (req, res) => {
     throw new ForbiddenError("Invalid or expired refresh token");
   }
   await db.refreshToken.deleteToken(tokenRefresh, storedToken.userId);
+
+  res.clearCookie("accessToken", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "Strict",
+  });
 
   return res.status(200).json({ message: "User logged out successfully" });
 });
